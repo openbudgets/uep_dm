beb15a6d64d79fd9256a20c248b45a1ff7a33e4a
wang <wk0206@gmail.com>
2017-05-11 15:40:00 +0200

    update test file

--- tests/test_uep_server.py
+++ tests/test_uep_server.py
@@ -9,15 +9,21 @@ import unittest
 class SendDMRequestToUEPServer(unittest.TestCase):
     """Basic test cases."""
 
-    def test_send_csv_to_uep_server(self):
-        #test file location, use your own 
-        #CSV_FILE = "/home/wang/OP/DAM/Input_SN4KQV.csv"  # path to the CSV file
-        CSV_FILE = "./Data/testfile.csv"
-        result_dic = json.loads(uep_dm.send_request_to_UEP_server(CSV_FILE))
+    def test_send_csv_to_uep_server_rulemining(self):
+        #test file location, use your own
+        CSV_FILE = "../Data/esif.csv"
+        result_dic = json.loads(uep_dm.send_request_to_UEP_server(CSV_FILE,"simple"))
         assert 'rules' in result_dic.keys()
         assert 'task' in result_dic.keys()
         assert False
 
+    def test_send_csv_to_uep_server_outlier(self):
+        #test file location, use your own
+        CSV_FILE = "../Data/esif.csv"
+        result_dic = json.loads(uep_dm.send_request_to_UEP_server(CSV_FILE,"outlier"))
+        assert 'outlier' in result_dic.keys()
+        assert 'task' in result_dic.keys()
+        assert False
 
 if __name__ == '__main__':
     unittest.main()
